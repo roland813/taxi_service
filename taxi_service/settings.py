658a7f4364e2c2faa4e7df7cb82bd72fe1007acd
roland813 <olegkorostensky@gmail.com>
2022-08-08 18:15:38 +0300

    https://taxi-service-py.herokuapp.com add to allowed hosts

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-=^ro3d+2&^tb9t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "https://taxi-service-py.herokuapp.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
