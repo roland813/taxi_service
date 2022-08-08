ed5dfe6becaabc88a0902e70f8ccead26d4b3250
roland813 <olegkorostensky@gmail.com>
2022-08-08 18:41:00 +0300

    Added procfile and runtime.txt

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-=^ro3d+2&^tb9t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://taxi-service-py.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1",
+                 "taxi-service-py.herokuapp.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
